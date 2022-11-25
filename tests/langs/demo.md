53d4818b05e9104a70f1a3bfb488387760bffe62
Wansmer <wansmer@gmail.com>
2022-11-25 14:38:04 +0300

    Added tests

---
+++ tests/langs/demo.md
@@ -0,0 +1,24 @@
+```javascript
+const renderDemo = (how, to) => ({ quistion: how, aciton: to });
+const str = '123' + 123;
+const isComplite = str.length >= 6;
+```
+```lua
+local ts_utils, ok = pcall(require, 'nvim-treesitter.ts_utils')
+```
+```html
+<div data-parent="plugin" class="swap" id="_sw">How to swap attributes</div>
+```
+```vue
+<template>
+  <app-item :key="line.id" v-for="line in lines">
+    {{ line.content }}
+  </app-item>
+</template>
+<script setup>
+  const lines = [
+    { content: 'how', id: 1 },
+    { id: 2, content: 'to' },
+  ];
+</script>
+```
---
+++ tests/utils.lua
@@ -0,0 +1,28 @@
+local M = {}
+
+local a = vim.api
+
+local function open_ang_get_buf(path)
+  vim.cmd(':e ' .. path)
+  return a.nvim_get_current_buf()
+end
+
+function M._test(v)
+  local swaper = require('sibling-swap')
+  local desc = v.desc:format(v.lang, v.func)
+
+  it(desc, function()
+    local bufnf = open_ang_get_buf(v.path)
+    local expected =
+      a.nvim_buf_get_lines(bufnf, v.expected[1], v.expected[2], true)
+    a.nvim_win_set_cursor(0, v.cursor)
+
+    swaper[v.func]()
+
+    local result = a.nvim_buf_get_lines(bufnf, v.result[1], v.result[2], true)
+    a.nvim_buf_delete(bufnf, { force = true })
+    assert.are.same(expected, result)
+  end)
+end
+
+return M
