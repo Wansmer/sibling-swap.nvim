a7bdf51d1dc14af3b02a1358e94c15514fe865d9
Wansmer <wansmer@gmail.com>
2023-11-22 13:45:52 +0300

    add annotations to main module

--- lua/sibling-swap/init.lua
+++ lua/sibling-swap/init.lua
@@ -2,22 +2,28 @@ local settings = require('sibling-swap.settings')
 
 local M = {}
 
+---Swap node with left-side sibling
 M.swap_with_left = function()
   require('sibling-swap.swap').swap_with('left')
 end
 
+---Swap node with right-side sibling
 M.swap_with_right = function()
   require('sibling-swap.swap').swap_with('right')
 end
 
+---Swap node with left-side sibling and replace operator to opposite
 M.swap_with_left_with_opp = function()
   require('sibling-swap.swap').swap_with('left', true)
 end
 
+---Swap node with right-side sibling and replace operator to opposite
 M.swap_with_right_with_opp = function()
   require('sibling-swap.swap').swap_with('right', true)
 end
 
+---Setup `sibling-swap.nvim`
+---@param opts UserOpts
 M.setup = function(opts)
   settings._update_settings(opts)
 end
